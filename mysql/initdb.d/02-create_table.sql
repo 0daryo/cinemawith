b3f850a0011d9bf11b2ea666cf23c71b5d7bfc5e
0daryo <38380837+0daryo@users.noreply.github.com>
2019-04-26 00:24:23 +0900

    feat: sql-data (#5)

--- mysql/initdb.d/02-create_table.sql
+++ mysql/initdb.d/02-create_table.sql
@@ -1,9 +1,74 @@
-CREATE TABLE `rapid_go`.`users`
+CREATE TABLE `cinemawith`.`users`
 (
     id int not null primary key,
-	name varchar(255) not null,
-    sex varchar(255) not null,
-    enabled boolean not null,
+	uid varchar
+(255) not null,
+	pr varchar
+(255) not null,
+	name varchar
+(255) not null,
+	profile_url varchar
+(255) not null,
+    sex varchar
+(255) not null,
+    age int not null,
+    nickname varchar
+(255) not null,
+    favorite varchar
+(255) not null,
+    enabled boolean not null default true,
 	created_at int not null,
 	updated_at int not null
-)
+);
+
+CREATE TABLE `cinemawith`.`friends`
+(
+    id int not null primary key,
+    user_id int not null,
+    friend_id int not null,
+    enabled boolean not null default false,
+	created_at int not null,
+	updated_at int not null,
+    FOREIGN KEY
+(`user_id`)
+    REFERENCES `cinemawith`.`users`
+(`id`),
+FOREIGN KEY
+(`friend_id`)
+    REFERENCES `cinemawith`.`users`
+(`id`)
+);
+
+CREATE TABLE `cinemawith`.`movies`
+(
+    id int not null primary key,
+	title varchar
+(255) not null,
+	description varchar
+(255) not null,
+    genre varchar
+(255) not null,
+    url varchar
+(255) not null,
+    enabled boolean not null default true,
+	created_at int not null,
+	updated_at int not null
+);
+
+CREATE TABLE `cinemawith`.`users_movies`
+(
+    id int not null primary key,
+    user_id int not null,
+    movie_id int not null,
+    enabled boolean not null default false,
+	created_at int not null,
+	updated_at int not null,
+    FOREIGN KEY
+(`user_id`)
+    REFERENCES `cinemawith`.`users`
+(`id`),
+FOREIGN KEY
+(`movie_id`)
+    REFERENCES `cinemawith`.`movies`
+(`id`)
+);

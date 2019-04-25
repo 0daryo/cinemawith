31ec0043f82ae85542fa70bbdca95aaeb97cf572
ryotaro oda <plm23.ccc@gmail.com>
2019-04-26 00:21:27 +0900

    feat: sql-data

--- mysql/initdb.d/03-m_users.sql
+++ mysql/initdb.d/03-m_users.sql
@@ -1,9 +1,60 @@
-INSERT INTO `rapid_go`.users (
-    id,name,sex,enabled,created_at,updated_at
+INSERT INTO `
+cinemawith`.users
+(
+id,
+uid,
+pr,
+name,
+profile_url,
+sex,
+age,
+nickname,
+favorite,
+enabled,created_at,updated_at
 )
 VALUES
-    (1,'山田太郎','man',true,555555,555555),
-    (2,'佐藤二郎','man',true,555555,555555),
-    (3,'田中三郎','man',true,555555,555555),
-    (4,'林花子','woman',true,555555,555555),
-    (5,'村田涼子','woman',true,555555,555555);
+    (1, '1111', 'よろしく', '山田太郎', 'http', 0, 21, "yama", "batman", true, 555555, 555555),
+    (2, '2222', 'よろしく', '佐藤二郎', 'http', 0, 21, "yama", "batman", true, 555555, 555555),
+    (3, '3333', 'よろしく', '田中三郎', 'http', 0, 21, "yama", "batman", true, 555555, 555555);
+
+
+INSERT INTO `
+cinemawith`.movies
+(
+id,
+title,
+description,
+genre,
+url,
+enabled,created_at,updated_at
+)
+VALUES
+    (1, 'アベンジャーズ', 'アイアンマン', "アクション", "http", true, 555555, 555555),
+    (2, 'アイアンマン', 'アイアンマン', "アクション", "http", true, 555555, 555555),
+    (3, 'ダークナイト', 'アイアンマン', "アクション", "http", true, 555555, 555555);
+
+
+INSERT INTO `
+cinemawith`.friends
+(
+id,
+user_id,
+friend_id,
+enabled,created_at,updated_at
+)
+VALUES
+    (1, 1, 2, true, 555555, 555555),
+    (2, 2, 1, true, 555555, 555555);
+
+INSERT INTO `
+cinemawith`.users_movies
+(
+id,
+user_id,
+movie_id,
+enabled,created_at,updated_at
+)
+VALUES
+    (1, 1, 2, true, 555555, 555555),
+    (2, 2, 3, true, 555555, 555555),
+    (3, 3, 1, true, 555555, 555555);
